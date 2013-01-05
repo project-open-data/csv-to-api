20c74995bddb2367555e290f4e050e20f74dbaff
Benjamin J. Balter <ben@balter.com>
2013-01-05 12:23:58 -0500

    change variable name to match project name

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 require_once('class.csv-to-api.php');
 
 # Create a new instance of the Instant API class.
-$ia = new CSV_To_API();
+$api = new CSV_To_API();
 
 # Intercept the requested URL and use the parameters within it to determine what data to respond with.
-$ia->parse_query();
+$api->parse_query();
 
 # Gather the requested data from its CSV source, converting it into JSON, XML, or HTML.
-$ia->parse();
+$api->parse();
 
 # Send the JSON to the browser.
-echo $ia->output();
+echo $api->output();
